96ec89f89c463e4ec7723166429c16f2c390c716
ekaterina <kica111@mail.ru>
2022-10-01 18:05:02 +0800

    написана программа для решения задачи

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿void Massiv()
+{
+    Console.WriteLine("Введите любые наборы символов через пробел: ");
+    string[] text = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);  
+    string[] result = new string[text.Length];
+
+    for (int i = 0; i < text.Length; i++)
+    {
+        string str = text[i];
+        if (str.Length <= 3) 
+        {
+            result[i] = text[i];
+        }
+    }
+    Console.Write($"[{String.Join(" ", result)}]");
+}
+Massiv();
